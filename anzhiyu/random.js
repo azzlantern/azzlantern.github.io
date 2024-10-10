f371528ab256d36c0f72cfb1c9aef12d92a17ce6
azzlantern <azzlantern233@outlook.com>
2024-10-10 22:56:00 +0800

    Site updated: 2024-10-10 22:56:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/09/test/"];function toRandomPost(){
+var posts=["2024/10/09/博客重新搭好啦/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
