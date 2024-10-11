65b7743c9a3e130b7eb62b848ba54ed9a50c4481
azzlantern <azzlantern233@outlook.com>
2024-10-11 12:11:09 +0800

    Site updated: 2024-10-11 12:11:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/09/博客重新搭好啦/"];function toRandomPost(){
+var posts=["2024/10/11/Chinese-Culture/","2024/10/11/game/","2024/10/09/博客重新搭好啦/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
