90e3ffe99d182b2d39523e378024ade0f7a5b12f
azzlantern <azzlantern233@outlook.com>
2025-05-28 17:34:09 +0800

    Site updated: 2025-05-28 17:34:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/20/Archlinux-Windows-双系统安装笔记/","2024/10/09/博客重新搭好啦/","2024/11/25/泠鸢三专《自下而向上升》开箱/","2025/03/12/English-Class-Presentation-小米：MIUI到HyperOS的智能演进/","2025/04/24/English-Class-Presentation-川菜：来自天府之国的味蕾交响曲/","2025/02/12/用NapCat-Yunzai搭建一个QQ机器人并接入Gemini/"];function toRandomPost(){
+var posts=["2024/10/09/博客重新搭好啦/","2025/04/20/Archlinux-Windows-双系统安装笔记/","2024/11/25/泠鸢三专《自下而向上升》开箱/","2025/02/12/用NapCat-Yunzai搭建一个QQ机器人并接入Gemini/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
